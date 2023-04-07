0c6b827bffe64fe23125246df1c1f8dcfc3d6958
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-07 18:59:09 +0800

    Finished exercise 1.5

--- part1/exercises/courseinfo/src/App.js
+++ part1/exercises/courseinfo/src/App.js
@@ -2,7 +2,7 @@
 // Component rendering the Header. 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.header}</h1>
   )
 }; 
 
@@ -32,29 +32,31 @@ const Total = (props) => {
 }; 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
+      <Header header={course.name}/>
       <Content 
-        parts = {parts}
+        parts = {course.parts}
       />
-      <Total total={parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)}/>
+      <Total total={course.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)}/>
     </div>
   )
 }
