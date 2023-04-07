f8fc2935892728db87cb92e8bf8da49303398df5
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-07 17:13:12 +0800

    Commit React app for part 1

---
+++ part1/part1/src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+
+// Define a new React component called 'Hello' 
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{JSON.stringify(friends[0])}</p>
+      
+    </div>
+  )
+}
+
+
+export default App; 
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
+*/ 
