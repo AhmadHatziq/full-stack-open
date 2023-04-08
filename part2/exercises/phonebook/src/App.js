aea2877b5a25cee66a5c36398295049dc4f93743
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-08 21:50:47 +0800

    Finished exercise 2.6
    Implemented the addition of a person to the phonebook.

--- part2/exercises/phonebook/src/App.js
+++ part2/exercises/phonebook/src/App.js
@@ -6,19 +6,42 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  // Used to handle the event when the input name text field is changed. 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  // Used to process input when the button is clicked. 
+  const handleClick = (event) => {
+    event.preventDefault()
+
+    // Extract input text data and store the new person. 
+    const newPerson = {name: newName}
+    setPersons(persons.concat(newPerson))
+
+    // Clear input field 
+    setNewName('')
+
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      
       <form>
         <div>
-          name: <input />
+          name: <input value={newName}
+                  onChange={handleNameChange}  
+                />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleClick}>add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p key={person.name}>{person.name} </p> )}
+
     </div>
   )
 }
