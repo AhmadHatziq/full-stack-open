d8ae199e12d7818ce9413a91b8aadc6c8eb91c64
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-08 11:57:31 +0800

    Finished exercise 1.8
    Refactor the application so that displaying the statistics is extracted into its own Statistics component.

--- part1/exercises/unicafe/src/App.js
+++ part1/exercises/unicafe/src/App.js
@@ -21,36 +21,43 @@ const DisplayComponent = ({display_label, value}) => {
   
 }
 
+// Calculates & displays the statistics
+const Statistics = ({good, neutral, bad}) => {
+
+    // Calculate the toal, average and positive values 
+    let total = good + neutral + bad 
+    let average = (good - bad * 1.0) / (1.0 * total)
+    let positive = good / total * 100.0
+
+    return(
+      <>
+        <DisplayComponent display_label="good" value={good}/>
+        <DisplayComponent display_label="neutral" value={neutral}/>
+        <DisplayComponent display_label="bad" value={bad}/>
+        <DisplayComponent display_label="all" value={total}/>
+        <DisplayComponent display_label="average" value={average}/>
+        <DisplayComponent display_label="positive" value={positive} />
+      </>
+    )
+
+
+}; 
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  // Calculate the toal, average and positive values 
-  let total = good + neutral + bad 
-  let average = (good - bad * 1.0) / (1.0 * total)
-  let positive = good / total * 100.0
   
-
   return (
     <div>
       <h1>give feedback</h1>
-
       <ButtonComponent button_label="good" value={good} setValue={setGood}/>
       <ButtonComponent button_label="neutral" value={neutral} setValue={setNeutral}/>
       <ButtonComponent button_label="bad" value={bad} setValue={setBad}/>
       
-
       <h1>statistics</h1>
-
-      <DisplayComponent display_label="good" value={good}/>
-      <DisplayComponent display_label="neutral" value={neutral}/>
-      <DisplayComponent display_label="bad" value={bad}/>
-      <DisplayComponent display_label="all" value={total}/>
-      <DisplayComponent display_label="average" value={average}/>
-      <DisplayComponent display_label="positive" value={positive} />
-
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
