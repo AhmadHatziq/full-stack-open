6d38be92941d0ccb21243d26606d00f5f940a642
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-09 10:44:36 +0800

    Enabled data to be read from server using useEffect() & axios

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,13 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [showAll, setShowAll] = useState(true)
-
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
+  const [newNote, setNewNote] = useState('') 
 
   // If true, will be 'notes'. Else, will only be notes where 'important' is set to true. 
   const notesToShow = showAll
@@ -34,6 +32,16 @@ const App = (props) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+    }, [])
 
   return (
     <div>
