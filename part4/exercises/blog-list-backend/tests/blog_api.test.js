8413d8300877452f04aec1b01c7d3451d1495756
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-16 20:55:02 +0800

    Add test for number of blog posts received from test DB
    Use the supertest package for writing a test that makes an HTTP GET request to the /api/blogs URL. Verify that the blog list application returns the correct amount of blog posts in the JSON format.

--- part4/exercises/blog-list-backend/tests/blog_api.test.js
+++ part4/exercises/blog-list-backend/tests/blog_api.test.js
@@ -5,16 +5,24 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const helper = require('./list_helper.test')
 
-// Initialize data to the tests DB. 
+// Clear & initialize data for the tests DB. 
 beforeEach(async() => {
   await Blog.deleteMany({})
   await Blog.insertMany(helper.initialBlogs)
 })
 
+test('correct number of blog posts returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
 
 test('notes are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
 })
