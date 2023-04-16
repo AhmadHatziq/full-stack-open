f06b5f107332a6546a44d240b0ae8eb31c8666a7
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-16 21:29:50 +0800

    Finished exercise 4.10
    Write a test that verifies that making an HTTP POST request to the /api/blogs URL successfully creates a new blog post.

--- part4/exercises/blog-list-backend/tests/blog_api.test.js
+++ part4/exercises/blog-list-backend/tests/blog_api.test.js
@@ -4,11 +4,41 @@ const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 const helper = require('./list_helper.test')
+const blog = require('../models/blog')
 
 // Clear & initialize data for the tests DB. 
 beforeEach(async() => {
   await Blog.deleteMany({})
   await Blog.insertMany(helper.initialBlogs)
+})
+
+test('Verifies that a new blog post can be added', async () => {
+  const newBlog = {
+    _id: '5a422a851b54a676234d17f6',
+    title: 'New blog title',
+    author: 'Person',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 1,
+    __v: 0
+  }
+
+  // POST the newBlog
+  await api 
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  // Get all the blogs from the DB
+  const blogs = (await Blog.find({})).map(blog => blog.toJSON())
+
+  // Check that length has increased by 1
+  expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
+
+  // Check that the new blog title is present
+  const titles = blogs.map(blog => blog.title)
+  expect(titles).toContain('New blog title')
+
 })
 
 test('Verifies that the unique identifier property of blogPosts is named "id"', async () => {
