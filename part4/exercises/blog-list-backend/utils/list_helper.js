2e779df96aeaadb9df80f3feae9fc873e29e1911
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-16 15:25:53 +0800

    Finished exercise 4.4
    Created a new totalLikes function and its corresponding tests

--- part4/exercises/blog-list-backend/utils/list_helper.js
+++ part4/exercises/blog-list-backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1 
 }
 
+// Receives a list of blogs as a parameter, returns the total sum of likes. 
+const totalLikes = (blogs) => {
+  const sumLikes = blogs.reduce((accumulator, blog) => {
+    return accumulator + blog.likes
+  }, 0)
+  return sumLikes
+}
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes 
 }
