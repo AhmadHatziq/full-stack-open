cb8205bd04ec412f532adf0cc859c4a9f1e92ae7
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-16 13:23:00 +0800

    Added Blog schema, GET & POST routes
    Routes currently work with POSTMAN

--- part4/exercises/blog-list-backend/index.js
+++ part4/exercises/blog-list-backend/index.js
@@ -1,1 +1,55 @@
-console.log('hello world')
+// Import statements 
+require('dotenv').config() 
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+// App declarations 
+const app = express() 
+app.use(cors())
+app.use(express.json())
+
+// Mongoose definitions 
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+const Blog = mongoose.model('Blog', blogSchema)
+const mongoUrl = process.env.MONGODB_URI
+
+// Connect to MongoDB
+console.log('connecting to', mongoUrl)
+mongoose.connect(mongoUrl)
+  .then( () => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+// Make the server listen on port 3001 or the environment variable (for deployment)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
