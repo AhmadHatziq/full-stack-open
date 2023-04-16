4d1b293b140b7ca15b40649b77995ec35da91a56
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-16 15:40:09 +0800

    Finished exercise 4.4
    Define a new totalLikes function that receives a list of blog posts as a parameter. The function returns the total sum of likes in all of the blog posts..
    
    Added corresponding tests as well.

--- part4/exercises/blog-list-backend/utils/list_helper.js
+++ part4/exercises/blog-list-backend/utils/list_helper.js
@@ -10,7 +10,21 @@ const totalLikes = (blogs) => {
   return sumLikes
 }
 
+// Receives a list of blogs and returns the blog with the most likes. 
+// If there are ties, return only one of the top blogs. 
+const favoriteBlog = (blogs) => {
+
+  const initialAccumulator = blogs[0] 
+
+  const highestLikedBlog = blogs.reduce((accumulator, currentBlog) => {
+    if (currentBlog.likes > accumulator.likes) return currentBlog 
+    else return accumulator 
+  }, initialAccumulator)
+
+  return highestLikedBlog
+
+}
 
 module.exports = {
-  dummy, totalLikes 
+  dummy, totalLikes, favoriteBlog
 }
