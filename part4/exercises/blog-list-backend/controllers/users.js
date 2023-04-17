8745683e0d526d5cbd657007df44015bc59d0c4e
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-17 18:18:58 +0800

    Create POST route for creating new users
    Implement a way to create new users by doing an HTTP POST request to address api/users

--- part4/exercises/blog-list-backend/controllers/users.js
+++ part4/exercises/blog-list-backend/controllers/users.js
@@ -1,11 +1,31 @@
 const logger = require('../utils/logger')
 const usersRouter = require('express').Router() 
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
-// GET route for all blog posts
+// GET route for all users
 usersRouter.get('/', async (request, response) => {
   logger.info('Hello from users')
   
+})
+
+// POST route 
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+  logger.info(`Saved user: ${JSON.stringify(savedUser)}`)
+
+  response.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
