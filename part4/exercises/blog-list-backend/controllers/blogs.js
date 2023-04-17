8d0fee6969cbef7d2060b0325ecd11043649c0f5
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-17 22:11:44 +0800

    Finished exercise 4.19
    Modify adding new blogs so that it is only possible if a valid token is sent with the HTTP POST request

--- part4/exercises/blog-list-backend/controllers/blogs.js
+++ part4/exercises/blog-list-backend/controllers/blogs.js
@@ -2,6 +2,20 @@ const logger = require('../utils/logger')
 const blogsRouter = require('express').Router() 
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+// Returns the JWT token from the authentication header bearer scheme 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  console.log(authorization)
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  if (authorization && authorization.startsWith('bearer ')) {
+    return authorization.replace('bearer ', '')
+  }
+  return null
+}
 
 // GET route for all blog posts
 blogsRouter.get('/', async (request, response) => {
@@ -14,16 +28,20 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   logger.info(`Received via POST: ${JSON.stringify(request.body)}`)
 
+  // Gets the user from the JWT token
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   // Create a blog object with the user id
-  const allUsers = (await User.find({})).map(u => u.toJSON())
-  const firstUser = allUsers[0]
-  const blogWithUser = new Blog({...request.body, user: firstUser.id})
+  const blogWithUser = new Blog({...request.body, user: user.id})
 
   // Save the blog with the user, update the user 
   const newBlog = await blogWithUser.save() 
-  const ownerUser = await User.findById(firstUser.id)
-  ownerUser.blogs = ownerUser.blogs.concat(newBlog.id)
-  await ownerUser.save()
+  user.blogs = user.blogs.concat(newBlog.id)
+  await user.save()
   logger.info(`Saved new Blog: ${JSON.stringify(newBlog)}`)
   response.status(201).json(newBlog)
    
