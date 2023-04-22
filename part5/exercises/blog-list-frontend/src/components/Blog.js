32bdc6ac50b0d66d263718c679cb48ccc9343c76
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-22 13:25:37 +0800

    Changed display for blog properties

--- part5/exercises/blog-list-frontend/src/components/Blog.js
+++ part5/exercises/blog-list-frontend/src/components/Blog.js
@@ -1,6 +1,16 @@
 const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+  <div  style={{ 
+    border: "1px solid black",
+    borderRadius: "5px",
+    padding: "10px",
+    margin: "10px"
+  }}>
+    <p><strong>Title:</strong> {blog.title}</p>
+    <p><strong>Author:</strong> {blog.author}</p>
+    <p><strong>URL:</strong> {blog.url}</p>
+    <p><strong>Likes:</strong> {blog.likes}</p>
+    <p><strong>User:</strong> {blog.user.username}</p>
+    
   </div>  
 )
 
