1a02d9ad7982e960d9d861fd0d7cfd0ce0a50001
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-24 11:05:54 +0800

    Finished exercise 5.10
    Modify the application to list the blog posts by the number of likes

--- part5/exercises/blog-list-frontend/src/App.js
+++ part5/exercises/blog-list-frontend/src/App.js
@@ -159,7 +159,7 @@ const App = () => {
     updatedBlogs[updatedBlogIndex] = updatedBlog
     setBlogs(updatedBlogs)
 
-  }
+  } 
 
   return (
     <div>
@@ -190,10 +190,8 @@ const App = () => {
         </Togglable>
       }
 
-
-
       <h2>Blogs</h2>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} handleLikes={handleLikes} />
       )}
     </div>
