29f464055c4165aa335cfcf994f3cd988e9f9361
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-30 00:10:12 +0800

    Added IDs for username, password and login button elements

--- part5/exercises/blog-list-frontend/src/App.js
+++ part5/exercises/blog-list-frontend/src/App.js
@@ -47,6 +47,7 @@ const App = () => {
           type="text"
           value={username}
           name="Username"
+          id="username"
           onChange={({ target }) => setUsername(target.value)}
         />
       </div>
@@ -56,10 +57,11 @@ const App = () => {
           type="password"
           value={password}
           name="Password"
+          id="password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button type="submit" id="login-button">login</button>
     </form>      
   )
 
