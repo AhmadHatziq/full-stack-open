ddfab2932e0984660ef99febf52f6182d7446e97
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-29 18:28:50 +0800

    Add a new route for resetting the TEST DB when testing
    TEST DB will reset when a POST request is made to "<PORT_NUM>/api/testing/reset"
    Eg "http://localhost:3003/api/testing/reset"

--- part4/exercises/blog-list-backend/index.js
+++ part4/exercises/blog-list-backend/index.js
@@ -2,6 +2,11 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
+// Check if running in test mode 
+if (process.env.NODE_ENV === 'test') {
+  logger.info(`Running in TEST mode.`)
+}
+
 // Make the server listen on port 3003 or the environment variable (for deployment)
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
--- part4/exercises/blog-list-backend/utils/config.js
+++ part4/exercises/blog-list-backend/utils/config.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 
+// Environment variables are stored in the ".env" file, which is not comitted. 
+
 const PORT = process.env.PORT
+
+// If running in TEST mode, will access the TEST DB. Else, will use the normal DB. 
 const MONGODB_URI = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI 
