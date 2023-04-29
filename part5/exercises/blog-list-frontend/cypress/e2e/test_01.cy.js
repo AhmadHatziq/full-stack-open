a94e1f2b480a05be6e8b0c12da6adb6f6479fe82
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-30 00:10:43 +0800

    Add the creating of a test user and a test for invalid credentials

--- part5/exercises/blog-list-frontend/cypress/e2e/test_01.cy.js
+++ part5/exercises/blog-list-frontend/cypress/e2e/test_01.cy.js
@@ -2,7 +2,6 @@ describe('Blog List App', () => {
 
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
   })
 
   it(`The string "Blogs" exists`, () => {
@@ -16,4 +15,41 @@ describe('Blog List App', () => {
     cy.contains('username')
     cy.contains('password') 
   })
+})
+
+describe('Login', () => {
+
+  // Reset the TEST DB and create a new user 
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const testUser = {
+      'name': 'testUser', 
+      'username': 'testUser', 
+      'password': 'testPassword'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', testUser)
+    
+  })
+
+  it('Fails with wrong credentials', () => {
+    cy.visit('http://localhost:3000')
+
+    // Login with incorrect credentials 
+    const correctUsername = 'testUser'
+    const wrongPassword = 'password'
+    cy.get('#username').type(correctUsername)
+    cy.get('#password').type(wrongPassword)
+    cy.get('#login-button').click()
+
+    // There should be an error message with the string 'wrong username or password'
+    const expectedErrorMessage = 'wrong username or password'
+    cy.contains(expectedErrorMessage)
+  })
+
+  it('Succeeds with correct credentials', () => {
+    
+  })
+
+
 })
