a3eec32275cf78892674c008c0582cf27487b6ce
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-04-29 18:43:29 +0800

    Finished exercise 5.17
    Made a test for checking that the application displays the login form by default.

--- part5/exercises/blog-list-frontend/cypress/e2e/test_01.cy.js
+++ part5/exercises/blog-list-frontend/cypress/e2e/test_01.cy.js
@@ -1,6 +1,19 @@
 describe('Blog List App', () => {
-  it('passes', () => {
+
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it(`The string "Blogs" exists`, () => {
     cy.visit('http://localhost:3000')
     cy.contains('Blogs')
+  })
+
+  it(`Displays the login form by default`, () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('login')
+    cy.contains('username')
+    cy.contains('password') 
   })
 })
