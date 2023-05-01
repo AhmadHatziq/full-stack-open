a2ee343e7dbfc4392d010f2807bd5eb04fc5f786
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-01 18:27:30 +0800

    Finished exercise 6.8
    Refactored the rendering of the anecdote list into a component called AnecdoteList.

--- part6/exercises/anecdotes-redux/src/App.js
+++ part6/exercises/anecdotes-redux/src/App.js
@@ -1,39 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { upVote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-
-  // useSelector will retrieve the state from the store
-  const anecdotes = useSelector(state => state)
-
-  // useDispatch is used to issue out actions to change the store state. 
-  // It will pass the 'action' to the 'reducer' function defined in the store. 
-  const dispatch = useDispatch()
-
-  // Dispatches upVote, with a specified ID. 
-  // Dispatch needs an object with the 'action' property. 
-  const vote = (id) => {
-    dispatch(upVote(id))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-
+      <AnecdoteList/>
       <AnecdoteForm/>
-
     </div>
   )
 }
---
+++ part6/exercises/anecdotes-redux/src/components/AnecdoteList.js
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { upVote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+
+   // useSelector will retrieve the state from the store
+   const anecdotes = useSelector(state => state)
+
+  // useDispatch is used to issue out actions to change the store state. 
+  // It will pass the 'action' to the 'reducer' function defined in the store. 
+  const dispatch = useDispatch()
+
+  // Dispatches upVote, with a specified ID. 
+  // Dispatch needs an object with the 'action' property. 
+  const vote = (id) => {
+    dispatch(upVote(id))
+  }
+
+  return (<>
+    {anecdotes.map(anecdote =>
+      <div key={anecdote.id}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => vote(anecdote.id)}>vote</button>
+        </div>
+      </div>
+    )}
+  </>)
+}
+
+export default AnecdoteList
