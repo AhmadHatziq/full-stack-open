6a76141674d7e682e0d5cb019fa4f1cda9e3723c
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-01 23:27:06 +0800

    Finished exercise 6.9
    Implement filtering for the anecdotes that are displayed to the user

--- part6/exercises/anecdotes-redux/src/components/AnecdoteList.js
+++ part6/exercises/anecdotes-redux/src/components/AnecdoteList.js
@@ -4,8 +4,17 @@ import { upVote } from '../reducers/anecdoteReducer'
 const AnecdoteList = () => {
 
   // useSelector will retrieve the state from the store
+  // Use the filter state value to filter the anecdotes 
   const anecdotes = useSelector(state => {
-    return state.anecdotes
+    
+    if (state.filter === '' || state.filter.length === 0) {
+      return state.anecdotes
+    }
+
+    // Return the filtered anecdotes 
+    const filteredAnecdotes = state.anecdotes.filter(obj => obj.content.toLowerCase().includes(state.filter))
+
+    return filteredAnecdotes
   })
 
   // useDispatch is used to issue out actions to change the store state. 
