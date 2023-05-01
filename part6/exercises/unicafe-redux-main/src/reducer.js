a698ee0836c17238d030566b9abb15e674a04ee4
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-01 15:40:19 +0800

    Finished exercise 6.2
     Implement the actual functionality of the application.

--- part6/exercises/unicafe-redux-main/src/reducer.js
+++ part6/exercises/unicafe-redux-main/src/reducer.js
@@ -6,18 +6,26 @@ const initialState = {
 
 // State is the default state. 
 const counterReducer = (state = initialState, action) => {
-  console.log('Action', action)
+  
   switch (action.type) {
 
     // Increment 'good' by 1 
     case 'GOOD':
       return { ...state, good: state.good + 1 }
+
+    // Increment 'ok' bu 1 
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 }
+
+    // Increment 'bad' by 1 
     case 'BAD':
-      return state
+      return { ...state, bad: state.bad + 1 }
+
+    // Reset all to 0. 
     case 'ZERO':
-      return state
+      return initialState
+
+    // Return the default state if there is an undefined action. 
     default: return state
   }
   
