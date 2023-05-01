beb70ada178af03ccb0db92f40317b931fa9e4e2
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-01 16:47:54 +0800

    Add comments

--- part6/exercises/anecdotes-redux/src/index.js
+++ part6/exercises/anecdotes-redux/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import App from './App'
 import reducer from './reducers/anecdoteReducer'
 
+// Create the store with the reducer. Initial state is in the reducer. 
 const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
