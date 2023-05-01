b8873c37abb6b0f76d46dbdda8acb65bedc4b955
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-01 22:27:49 +0800

    Created skeleton files for Filter reducer and component

---
+++ part6/exercises/anecdotes-redux/src/components/Filter.js
@@ -0,0 +1,22 @@
+const Filter = () => {
+  
+  const handleChange = (event) => {
+    
+    // input-field value is in variable event.target.value
+    const filterValue = event.target.value
+  
+    
+    
+  }
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter
