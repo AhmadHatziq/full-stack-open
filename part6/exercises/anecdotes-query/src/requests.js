6471608ae732937f73f5a1f6263327f24b664d66
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-14 17:07:16 +0800

    Finished exercise 6.22
    Implement voting for anecdotes using React Query.
    
    The application will automatically render the increased number of votes for the voted anecdote.

--- part6/exercises/anecdotes-query/src/requests.js
+++ part6/exercises/anecdotes-query/src/requests.js
@@ -2,8 +2,32 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
+// Used to obtain all anecdotes
 export const getAnecdotes = () =>
   axios.get(baseUrl).then(res => res.data)
 
+// Used to create a new anecdote 
 export const createAnecdote = newAnecdote =>
   axios.post(baseUrl, newAnecdote).then(res => res.data)
+
+// Used to update an anecdote 
+export const updateAnecdote = updatedAnecdote =>
+  axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(res => res.data)
+
+// Used to upvote a new anecdote  
+export const upvoteAnecdote = async (anecdoteId) => {
+
+  const anecdoteUrl = `${baseUrl}/${anecdoteId}`
+
+  // Get the current vote count 
+  const currentAnecdote = (await axios.get(anecdoteUrl)).data 
+  const currentVotes = parseInt(currentAnecdote.votes)
+
+  // Create a new anecdote, with the incremented vote count 
+  const newAnecdote = {...currentAnecdote, 'votes': currentVotes + 1}
+
+  // Update the new anecdote to the backend 
+  const response = await axios.put(anecdoteUrl, newAnecdote)
+  return response.data 
+
+}
