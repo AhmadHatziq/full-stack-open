18ccfeb838a3a2274d8cac3ade7cb99addfc4b14
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-14 09:45:21 +0800

    Finished exercise 6.20
    Implement retrieving anecdotes from the server using React Query

--- part6/exercises/anecdotes-query/src/App.js
+++ part6/exercises/anecdotes-query/src/App.js
@@ -1,19 +1,24 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
+import { useQuery } from 'react-query' 
+import { getAnecdotes } from './requests'
+
+
 const App = () => {
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+
+  // Obtain and set the anecdotes using React Query. 
+  // Note that in index.js, the App component must be wrapped in '<QueryClientProvider client={queryClient}>'
+  const result = useQuery('anecdotes', getAnecdotes, {retry: 1})
+  if ( result.isLoading ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+  const anecdotes = result.data
 
   return (
     <div>
