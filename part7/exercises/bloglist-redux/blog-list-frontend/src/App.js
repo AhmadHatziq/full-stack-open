287b831201d3b06bc3bd4b213a3fea44c25822fc
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-24 16:37:27 +0800

    Updated imports

--- part7/exercises/bloglist-redux/blog-list-frontend/src/App.js
+++ part7/exercises/bloglist-redux/blog-list-frontend/src/App.js
@@ -24,7 +24,6 @@ import BlogDisplay from "./components/BlogDisplay";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 const baseUrl = "http://localhost:3003/api/blogs";
 
