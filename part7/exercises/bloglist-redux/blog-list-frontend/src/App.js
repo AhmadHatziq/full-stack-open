04cf259277c7d9d2ebf8502b798363e1fc060a21
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-24 13:14:50 +0800

    Add the skeleton for a nav bar using react-bootstrap

--- part7/exercises/bloglist-redux/blog-list-frontend/src/App.js
+++ part7/exercises/bloglist-redux/blog-list-frontend/src/App.js
@@ -21,6 +21,10 @@ import {
 } from "./reducers/blogReducer";
 import { setUser, initializeUsers } from "./reducers/userReducer";
 import BlogDisplay from "./components/BlogDisplay";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const baseUrl = "http://localhost:3003/api/blogs";
 
@@ -350,6 +354,24 @@ const App = () => {
     );
   };
 
+  // Display the navigation bar
+  const displayNavBar = () => {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  };
+
   // Obtain user ID
   const userIdMatch = useMatch("/users/:userId");
 
@@ -358,6 +380,7 @@ const App = () => {
 
   return (
     <div>
+      {displayNavBar()}
       <Routes>
         <Route path="/users" element={displayUsers()}></Route>
         <Route path="/" element={displayMainPage()}></Route>
