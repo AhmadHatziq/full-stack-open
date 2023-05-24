475aa53b56edefed8826a18da9d686dc991128bb
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-05-24 11:34:14 +0800

    Add skeleton for single BlogDisplay component

---
+++ part7/exercises/bloglist-redux/blog-list-frontend/src/components/BlogDisplay.js
@@ -0,0 +1,11 @@
+// Renders information regarding a single blog post on its own
+const BlogDisplay = ({ blog }) => {
+  return (
+    <>
+      <h1>Blog title</h1>
+      <p>Blog contents</p>
+    </>
+  );
+};
+
+export default BlogDisplay;
