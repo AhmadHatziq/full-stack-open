478efc53b8ca17bfedc6c4cda7e3ca86e1131fa3
Hatziq <hatziq@u.nus.edu>
2024-09-25 20:05:54 +0800

    Add error handling for new Person with existing name

--- part8/phonebook-app-a/index.js
+++ part8/phonebook-app-a/index.js
@@ -2,6 +2,7 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
 
 let persons = [
   {
@@ -72,6 +73,17 @@ const resolvers = {
 
   Mutation: {
     addPerson: (root, args) => {
+
+      // Check if person already exists. 
+      if (persons.find(p => p.name === args.name)) {
+        throw new GraphQLError('Name must be unique', {
+          extensions: {
+            code: 'BAD_USER_INPUT_FOR_NAME', 
+            invalidArgs: args.name 
+          }
+        })
+      }
+
       const person = { ...args, id: uuid() }
       persons = persons.concat(person)
       return person
